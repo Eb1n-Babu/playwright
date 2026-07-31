ad13d7cd1c726f070a21ac6a975c906dd7f968e7
Eb1n-Babu <ebin.babu.in@icloud.com>
2026-07-31 20:32:08 +0400

    project updated: select drop down implemented and some files deleted

---
+++ PlayWright_Tutorial_pdf_based/Selecting_dropdowns.py
@@ -0,0 +1,18 @@
+from playwright.async_api import async_playwright
+import asyncio
+
+
+async def main():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
+        context = await browser.new_context(viewport={"width": 1920, "height": 1080})  # type: ignore
+        page = await context.new_page()
+        await page.goto("https://artifactgeeks.com/blog/testing-tools/handling-dropdown-in-playwright")
+        await page.locator("//a[text()='Solutions']").click()
+        await page.locator("//a[text()='Students']").click()
+        await page.wait_for_timeout(20000)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
